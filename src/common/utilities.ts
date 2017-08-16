db7fb5728fda1b30844c94e5c5525e020a282f9b
Kurtis Best <dacobra3@hotmail.com>
2017-08-15 22:22:10 -0400

    remove guid method fro utilities; prep v0.2.0

--- src/common/utilities.ts
+++ src/common/utilities.ts
@@ -9,19 +9,6 @@ export class Utilities {
     return `${Date.now().toString()}${Math.floor((Math.random() * 10000000000000000))}`;
   }
 
-  /**Create a guid */
-  public static guid(): string {
-    return Utilities.guid_s4() + Utilities.guid_s4() + '-' + Utilities.guid_s4() + '-' + Utilities.guid_s4() + '-' +
-      Utilities.guid_s4() + '-' + Utilities.guid_s4() + Utilities.guid_s4() + Utilities.guid_s4();
-  }
-
-  /**Aids in creating a guid */
-  private static guid_s4(): string {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-
   /**
    * Gets the property name of the passed property
    * @param property Field/property name to retrieved
